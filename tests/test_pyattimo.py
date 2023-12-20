5aeef0615559d857def1a8a8e9b9f5b70f224a0a
Patrick Schäfer <patrick.schaefer@informatik.hu-berlin.de>
2023-12-20 11:49:31 +0100

    updates

---
+++ tests/test_pyattimo.py
@@ -0,0 +1,39 @@
+import pyattimo
+from motiflets.plotting import *
+
+
+def test_attimo():
+    # load a dataset, any list of numpy array of floats works fine
+    # The following call loads the first 100000 points of the ECG
+    # dataset (which will be downloaded from the internet)
+    ts = pyattimo.load_dataset('ecg', 100_000)
+    print("Size of DS: ", ts.shape)
+
+    # Now we can find k-motiflets:
+    #  - w is the window length
+    #  - support is the number of subsequences in the motiflet (k in the motiflet paper)
+    #  - repetitions is the number of LSH repetitions
+    start = time.time()
+
+    m_iter = pyattimo.MotifletsIterator(
+        ts, w=1000, max_k=30
+    )
+
+    end = time.time()
+    print("Discovered motiflets in", end - start, "seconds")
+
+    for m in m_iter:
+        print(m)
+        print(m.extent)
+        # np.sort(m.indices)
+
+def test_motiflets():
+    # load a dataset, any list of numpy array of floats works fine
+    # The following call loads the first 100000 points of the ECG
+    # dataset (which will be downloaded from the internet)
+    ts = pyattimo.load_dataset('ecg', 100_000).flatten()
+    print("Size of DS: ", ts.shape)
+
+    k=10
+    mot = Motiflets("ECG", ts)
+    _ = mot.fit_k_elbow(k, 1000)
