eb7083a7ad452b161a4eb8bf6ae3fc344be7f91b
Patrick Schäfer <patrick.schaefer@informatik.hu-berlin.de>
2023-12-27 15:15:56 +0100

    update

---
+++ tests/test_univariate_synthetic.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+from motiflets.plotting import *
+from scipy.stats import zscore
+
+def generate_rectangular_wave(motif_length):
+    return (motif_length,
+            np.where(np.arange(motif_length) < motif_length / 2, 1, -1))
+
+
+def generate_sine_wave(motif_length):
+    sine_frequency = 0.05
+    sine_phase = np.pi / 2
+    sine_time = np.arange(motif_length)
+
+    # Generate sine wave
+    return np.sin(2 * np.pi * sine_frequency * sine_time + sine_phase).flatten()
+
+
+def test_random_walk():
+    # Parameters
+    ts_length = 1e7
+    motif_length = 1000
+    motif_cardinality = 10
+    random_walk_step_size = 0.1
+
+    # Generate random walk
+    time_series = np.random.normal(scale=random_walk_step_size, size=int(ts_length))
+    time_series = zscore(time_series)
+
+    for i in np.arange(0, motif_cardinality):
+        # Implant sine wave into random walk
+        sine = generate_sine_wave(motif_length)
+        # noise = np.random.normal(0.1, size=duration) * 0.1
+
+        # Choose the position to insert sine wave
+        start = int(i / motif_cardinality * ts_length )
+        time_series[start:start + motif_length] = sine  # + noise
+
+    ds_name = "Random Walk"
+    ml = Motiflets(ds_name=ds_name, series=time_series)
+    ml.plot_dataset()
+    ml.fit_k_elbow(k_max=motif_cardinality, motif_length=1000)
