9121c36d8f68f07b27604954ba38cabaa399a4a2
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2025-07-25 15:07:42 +0200

    updates

--- tests/test_momp_benchmarks.py
+++ tests/test_momp_benchmarks.py
@@ -10,30 +10,30 @@ import utils as ut
 path = "../datasets/momp/"
 
 filenames = {
-    # "Bird12-Week3_2018_1_10": ["22.5 hours of Chicken data at 100 Hz", 16384, ""],
-    # "BlackLeggedKittiwake": ["Flying Bird: Black‐legged Kittiwake", 8192, "?"],
-    # "Challenge2009Respiration500HZ": ["Challenge 2009 Respiration", 16384, "?"],
-    # "Challenge2009TestSetA_101a": ["Respiration", 4096, "?"],
-    # "CinC_Challenge": ["Electroencephalography C3-M2 Part 2", 8192, "Calibration"],
-    # "EOG_one_hour_50_Hz": ["EOG_one_hour_50_Hz", 2048, "?"],
-    # "EOG_one_hour_400_Hz": ["EOG_one_hour_400_Hz", 8192, "?"],
-    # "FingerFlexionECoG": ["Finger Flexion ECoG electrocorticography", 16384, "?"],
-    # "HAR_Ambient_Sensor_Data": ["Human Activity Recognition", 4096, "?"],
-    # "house": ["Household Electrical Demand", 32768, "?"],
+    "Bird12-Week3_2018_1_10": ["22.5 hours of Chicken data at 100 Hz", 16384, ""],
+    "BlackLeggedKittiwake": ["Flying Bird: Black‐legged Kittiwake", 8192, "?"],
+    "Challenge2009Respiration500HZ": ["Challenge 2009 Respiration", 16384, "?"],
+    "Challenge2009TestSetA_101a": ["Respiration", 4096, "?"],
+    "CinC_Challenge": ["Electroencephalography C3-M2 Part 2", 8192, "Calibration"],
+    "EOG_one_hour_50_Hz": ["EOG_one_hour_50_Hz", 2048, "?"],
+    "EOG_one_hour_400_Hz": ["EOG_one_hour_400_Hz", 8192, "?"],
+    "FingerFlexionECoG": ["Finger Flexion ECoG electrocorticography", 16384, "?"],
+    "HAR_Ambient_Sensor_Data": ["Human Activity Recognition", 4096, "?"],
+    "house": ["Household Electrical Demand", 32768, "?"],
     "Lab_FD_061014": ["Insect EPG - Flaming Dragon", 32768, "?"],
-    # "Lab_K_060314": ["ACP on Kryder Citrus", 65536, ""],
-    # "lorenzAttractorsLONG": ["Lorenz Attractors", 524288, "?"],
-    # "MGHSleepElectromyography": ["MGH Sleep Electromyography 200 Hz", 32768, "?"],
-    # "recorddata": ["EOG Example", 2048, "?"],
-    # "solarwind": ["Solar Wind", 32768, "?"],
-    # "SpainishEnergyDataset": ["SpainishEnergyDataset", np.nan, "?"],
-    # "SpainishEnergyDataset5sec": ["Spainish Energy Dataset 5 sec", 524288, "?"],
-    # "stator_winding": ["Electric Motor Temperature", 32768, "?"],
-    # "swtAttack7": ["Swat Attack 7", 4 * 16 * 256, "?"],
-    # "swtAttack38": ["Swat Attack 38", 16 * 256, "?"],
-    # "SynchrophasorEventsLarge": ["Synchrophasor Events Large", 256 * 2 * 128, "?"],
-    # "water": ["Water Demand", 8192, ""],
-    # "WindTurbine": ["Wind Turbine R24VMON Rotating system", 32768, "Precursor Dropout"]
+    "Lab_K_060314": ["ACP on Kryder Citrus", 65536, ""],
+    "lorenzAttractorsLONG": ["Lorenz Attractors", 524288, "?"],
+    "MGHSleepElectromyography": ["MGH Sleep Electromyography 200 Hz", 32768, "?"],
+    "recorddata": ["EOG Example", 2048, "?"],
+    "solarwind": ["Solar Wind", 32768, "?"],
+    "SpainishEnergyDataset": ["SpainishEnergyDataset", np.nan, "?"],
+    "SpainishEnergyDataset5sec": ["Spainish Energy Dataset 5 sec", 524288, "?"],
+    "stator_winding": ["Electric Motor Temperature", 32768, "?"],
+    "swtAttack7": ["Swat Attack 7", 4 * 16 * 256, "?"],
+    "swtAttack38": ["Swat Attack 38", 16 * 256, "?"],
+    "SynchrophasorEventsLarge": ["Synchrophasor Events Large", 256 * 2 * 128, "?"],
+    "water": ["Water Demand", 8192, ""],
+    "WindTurbine": ["Wind Turbine R24VMON Rotating system", 32768, "Precursor Dropout"]
 }
 
 
@@ -74,7 +74,7 @@ def read_mat(filename):
     data = data[np.isfinite(data)]
 
     # np.savetxt(path + "/csv/" + filename + ".csv", data[:100000], delimiter=",")
-    return data
+    return data.astype(np.float64)
 
 
 def test_motiflets_scale_n(
@@ -125,25 +125,26 @@ def main():
     for filename in filenames.keys():
         ds_name, length, meaning = filenames[filename]
         print (f"Running: {filename, ds_name}")
+        data = read_mat(filename)
 
         # pyattimo
         # backends = ["pyattimo"]
         # for delta in deltas:
         #    run_safe(
-        #        filename, read_mat(filename), l_range, k_max, backends, delta
+        #        filename, data, l_range, k_max, backends, delta
         #    )
 
         # scalable
         backends = ["scalable"]
-        run_safe(
-           filename, read_mat(filename), l_range, k_max, backends, subsampling=10
-        )
+        # run_safe(
+        #   filename, data, l_range, k_max, backends, subsampling=10
+        #)
 
         # # subsampling
         # backends = ["scalable"]
         # for subsampling in [8, 16]:
         #    run_safe(
-        #       filename, read_mat(filename),
+        #       filename, data,
         #       l_range=l_range, k_max=k_max, backends=backends, subsampling=subsampling
         #    )
 
